57ba4f3c1690d8ec55f8e15b20576786c77f54f7
Bram Tukker <bram@hyperbit.nl>
2021-04-01 05:40:05 +0200

    Changed write file callback in minify.js

--- minify.js
+++ minify.js
@@ -1,14 +1,17 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const minify = require('minify');
 
 minify(
   './dist/css/react-bootstrap-form.css'
-
 ).then((data) => {
-  fs.writeFileSync('./dist/css/react-bootstrap-form.min.css', data);
+  fs.writeFile(
+    './dist/css/react-bootstrap-form.min.css', data
+  ).catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 
 }).catch((error) => {
   console.error(error);
   process.exit(1);
-
 });
